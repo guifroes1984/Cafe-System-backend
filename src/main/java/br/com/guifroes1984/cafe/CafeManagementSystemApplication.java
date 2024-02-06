04125ffe7f41261377226a10bf161308c366aade
guifroes1984 <guilhermefroes26@gmail.com>
2024-02-06 00:05:10 -0300

    Start na API. Criado a classe User.

---
+++ src/main/java/br/com/guifroes1984/cafe/CafeManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package br.com.guifroes1984.cafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/guifroes1984/cafe/CafeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.guifroes1984.cafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
